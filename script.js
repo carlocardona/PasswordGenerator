fb1c02bb9828128f1c648d5d8919e4cf4f4b4c83
Carlo <pbjunkiep3@yahoo.com>
2020-06-03 20:13:25 +1000

    20:13,06:03

--- script.js
+++ script.js
@@ -9,5 +9,52 @@ function writePassword() {
     passwordText.value = password;
 }
 
+function generatePassword() {
+
+    let counter = 0;
+    let generatedPasswordHolder = [];
+
+    let userInput = prompt("How many characters for your password (8-128)");
+    let upperCase = confirm("Do you want UpperCase letters?");
+    let passNumbers = confirm("Do you want Numbers?");
+    let specialChar = confirm("Special Characters?");
+
+    do {
+
+        // random lower case
+        let randLettLower = Math.floor(Math.random() * 25) + 0;
+        let lowerAlph = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+        generatedPasswordHolder.push(lowerAlph[randLettLower]);
+
+        if (upperCase == true) {
+            //random upper case
+            let randLettUpper = Math.floor(Math.random() * 25) + 0;
+            let capsAlph = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+            generatedPasswordHolder.push(capsAlph[randLettUpper]);
+        }
+
+        if (passNumbers == true) {
+            //random number
+            let randNum = Math.floor(Math.random() * 9) + 0;
+            let numbs = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+            generatedPasswordHolder.push(numbs[randNum]);
+        }
+
+        if (specialChar == true) {
+            //random special character
+            let randChar = Math.floor(Math.random() * 25) + 0;
+            let specialChar = ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "{", "}", "~"];
+            generatedPasswordHolder.push(specialChar[randChar]);
+        }
+
+        counter++;
+    } while (counter <= userInput);
+
+    let arrayToSring = generatedPasswordHolder.toString();
+
+    return arrayToSring;
+
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
